638aaa1838abea805bc79fc1fa66ae7a667ea054
siddhantbothra <siddhantjain0911@gmail.com>
2022-06-29 18:23:51 +0530

    Adding Display Alert

--- app.js
+++ app.js
@@ -16,6 +16,27 @@ form.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault();
+  console.log(grocery.value);
+  const id = new Date().getTime();
+  const value = grocery.value;
+  if (value && !editFlag) {
+    console.log("adding item");
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    displayAlert("Please enter a value", "danger");
+  }
+}
+
+// display Alert function
+
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setInterval(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
 // ****** LOCAL STORAGE **********
 
